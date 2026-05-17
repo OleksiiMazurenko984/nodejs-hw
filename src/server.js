e5e2494f56fccfe1e864d1b4f1eae744391d5fff
Oleksii Mazurenko <rebornemptymail@gmail.com>
2026-05-17 19:35:43 +0300

    Add json and cors middlewares

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import 'dotenv/config';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
+
+app.use(express.json());
+app.use(cors());
 
 app.get('/notes', (req, res) => {
   res.status(200).json({ message: 'Retrieved all notes' });
