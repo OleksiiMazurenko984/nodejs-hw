adaa68db237e6a23543ea1c22d235c5a5020440a
Oleksii Mazurenko <rebornemptymail@gmail.com>
2026-05-17 19:37:53 +0300

    Add pino logs

--- src/server.js
+++ src/server.js
@@ -1,12 +1,29 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import pino from 'pino-http';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
 app.use(express.json());
 app.use(cors());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
 
 app.get('/notes', (req, res) => {
   res.status(200).json({ message: 'Retrieved all notes' });
