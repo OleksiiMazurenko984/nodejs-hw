0044e5521adba035ef362eb7629026f79b90879e
Oleksii Mazurenko <rebornemptymail@gmail.com>
2026-05-20 18:55:05 +0300

    Remove prod check from errorHandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,11 +1,7 @@
 export const errorHandler = (err, req, res, next) => {
   console.error(err);
 
-  const isProd = process.env.NODE_ENV === 'production';
-
   res.status(500).json({
-    message: isProd
-      ? 'Something went wrong. Please try again later.'
-      : err.message,
+    message: err.message,
   });
 };
