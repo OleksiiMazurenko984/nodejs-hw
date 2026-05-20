30b2b2eb92acd40be11a628b2ce90244c91e8a77
Oleksii Mazurenko <rebornemptymail@gmail.com>
2026-05-20 19:39:23 +0300

    Change naming of getNotes

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
-import { getNotes, getNoteById } from '../controllers/notesController.js';
+import { getAllNotes, getNoteById } from '../controllers/notesController.js';
 
 const router = Router();
 
-router.get('/notes', getNotes);
+router.get('/notes', getAllNotes);
 router.get('/notes/:noteId', getNoteById);
 
 export default router;
