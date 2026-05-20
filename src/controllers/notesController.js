3065c113dd87acf20cfd23f1732674aaeeffdf25
Oleksii Mazurenko <rebornemptymail@gmail.com>
2026-05-20 20:08:00 +0300

    Add delete request

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -21,3 +21,16 @@ export const createNote = async (req, res) => {
   const note = await Note.create(req.body);
   res.status(201).json(note);
 };
+
+export const deleteNote = async (req, res) => {
+  const { noteId } = req.params;
+  const note = await Note.findOneAndDelete({
+    _id: noteId,
+  });
+
+  if (!note) {
+    throw createHttpError(404, 'Note not found');
+  }
+
+  res.status(200).json(note);
+};
--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -3,6 +3,7 @@ import {
   getAllNotes,
   getNoteById,
   createNote,
+  deleteNote,
 } from '../controllers/notesController.js';
 
 const router = Router();
@@ -10,5 +11,6 @@ const router = Router();
 router.get('/notes', getAllNotes);
 router.get('/notes/:noteId', getNoteById);
 router.post('/notes', createNote);
+router.delete('/notes/:noteId', deleteNote);
 
 export default router;
