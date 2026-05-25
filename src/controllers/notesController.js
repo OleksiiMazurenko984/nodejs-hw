5800aaeef9b177713b47450d5b09ec157b29de2f
Oleksii Mazurenko <rebornemptymail@gmail.com>
2026-05-25 20:15:10 +0300

    Fix tag filtration and change naming of totalItems to totalNotes

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -9,7 +9,7 @@ export const getAllNotes = async (req, res) => {
   const notesQuery = Note.find();
 
   if (tag) {
-    notesQuery.where('tag').equals(TAGS);
+    notesQuery.where('tag').equals(tag);
   }
 
   if (search) {
@@ -21,17 +21,17 @@ export const getAllNotes = async (req, res) => {
     });
   }
 
-  const [totalItems, notes] = await Promise.all([
+  const [totalNotes, notes] = await Promise.all([
     notesQuery.clone().countDocuments(),
     notesQuery.skip(skip).limit(perPage),
   ]);
 
-  const totalPages = Math.ceil(totalItems / perPage);
+  const totalPages = Math.ceil(totalNotes / perPage);
 
   res.status(200).json({
     page,
     perPage,
-    totalItems,
+    totalNotes,
     totalPages,
     notes,
   });
