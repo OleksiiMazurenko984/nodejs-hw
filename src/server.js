8a784669cfebf02815fc3eefdaa7afc61b7d15bd
Oleksii Mazurenko <rebornemptymail@gmail.com>
2026-05-29 18:03:59 +0300

    Add user login logic

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 import { errors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
@@ -12,6 +13,10 @@ import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 const PORT = process.env.PORT ?? 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 app.use(logger);
 app.use(express.json());
