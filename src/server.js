f9e4de104ec2acfdf2b4e84f73e571dd9c4a29f8
Oleksii Mazurenko <rebornemptymail@gmail.com>
2026-05-30 21:53:38 +0300

    Remove doubling of cors and express.json

--- src/server.js
+++ src/server.js
@@ -14,13 +14,10 @@ import notesRoutes from './routes/notesRoutes.js';
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
-app.use(express.json());
-app.use(cors());
-app.use(cookieParser());
-
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 app.use(authRoutes);
 app.use(notesRoutes);
